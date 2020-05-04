065905d6b2fa19f48f9a33dee3c0e9f2fe450b05
Evaldas Mikalauskas <mikalauskas.e@gmail.com>
2020-05-04 22:12:29 +0300

    homepage posts in cards

--- client/src/components/login_signup/ForgotPassword.js
+++ client/src/components/login_signup/ForgotPassword.js
@@ -1,18 +1,28 @@
 import React, { Component } from "react";
 
 export default class ForgotPassword extends Component {
-    render() {
-        return (
-            <form>
-                <h3>Forgot Password</h3>
+  render() {
+    return (
+      <div id="container-wrapper" className="container-wrapper">
+        <div id="container-inner" className="container-inner">
+          <form>
+            <h3>Forgot Password</h3>
 
-                <div className="form-group">
-                    <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
-                </div>
+            <div className="form-group">
+              <label>Email address</label>
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Enter email"
+              />
+            </div>
 
-                <button type="submit" className="btn btn-primary btn-block">Submit</button>
-            </form>
-        );
-    }
+            <button type="submit" className="btn btn-primary btn-block">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
