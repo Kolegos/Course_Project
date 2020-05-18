163f872bee093265ad13e9b19e2fc168f30511cd
Evaldas Mikalauskas <mikalauskas.e@gmail.com>
2020-05-19 00:15:44 +0300

    admin account and restrictions for admin pages

---
+++ client/src/components/admin/AdminPanel.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AdminPanel = () => {
+  return (
+    <div className="container-wrapper">
+      <div className="container-inner">testing</div>
+    </div>
+  );
+};
+
+export default AdminPanel;
