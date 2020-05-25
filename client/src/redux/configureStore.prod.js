aade08c8b873c17ec827105e980a1b4f6e0f16d9
Evaldas Mikalauskas <mikalauskas.e@gmail.com>
2020-05-25 15:58:04 +0300

    fixed production

--- client/src/redux/configureStore.prod.js
+++ client/src/redux/configureStore.prod.js
@@ -1,7 +1,19 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers/index";
+import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
 import thunk from "redux-thunk";
 
 export default function configureStore(initialState) {
-  return createStore(rootReducer, initialState, thunk);
+  const composeEnhancers =
+    (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        trace: true,
+        traceLimit: 25,
+      })) ||
+    compose;
+  return createStore(
+    rootReducer,
+    initialState,
+    composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant()))
+  );
 }
